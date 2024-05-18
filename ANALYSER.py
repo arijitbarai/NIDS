45fee06d104aee08f34b731e726c6661c6cdfe32
arijitbarai <arijitbarau@gmail.com>
2024-05-18 19:22:49 +0530

    Initial commit

---
+++ ANALYSER.py
@@ -0,0 +1,35 @@
+import os
+import datetime
+import csv
+
+myPath = os.getcwd()
+files = os.listdir(myPath)
+
+
+
+for f in files:
+    if f.endswith("xyz"):
+        print(f)
+        fx = open(f)
+        lines = fx.readlines()
+        fx.close()
+        data =((lines[0]).split(','))
+        print(data)
+        mytype = data[0]
+        myname = data[1]
+
+        print(mytype+" -- "+myname)
+        mynewfilename = (mytype +"-"+str(datetime.date.today())+"-"+myname +"-"+(f.replace(".","-")).replace("xyz","cvs"))
+        new_file = open(mynewfilename, "w")
+        i=0
+        for line in lines:
+            if i == 0:
+                print(line)
+            else:
+                new_file.write(line)
+                print(i, line)
+            i += 1
+        new_file.close()
+        os.remove(f)
+
+print(" ")
---
+++ MALICIOUS_CHECK.py
@@ -0,0 +1,48 @@
+import csv
+import os
+import datetime
+
+myPath = os.getcwd()
+files = os.listdir(myPath)
+def check_malicious_processes():
+    # Read the CSV file containing the names of malicious processes
+    with open('malicious_processes.csv', 'r') as f:
+        reader = csv.reader(f)
+        malicious_processes = list(reader)
+
+    for f in files:
+        if f.startswith("PROINFO"):
+            print(f)
+            fx = open(f)
+            lines = fx.readlines()
+            fx.close()
+
+            # Extract the hostname and port from the filename
+            filename_parts = f.split('-')
+            now = datetime.datetime.now()
+
+            formatted_timestamp = now.strftime("%Y-%m-%d-%H-%M-%S")
+            # Create a new CSV file to store the malicious processes found
+            new_filename = 'MALINFO-'+formatted_timestamp+'.csv'
+            with open(new_filename, 'w', newline='') as csvfile:
+                fieldnames = ['PROCESSID', 'PROCESSNAME', 'STATUS', 'STARTTIME']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+                writer.writeheader()
+
+                for line in lines[1:]:  # Skip the header line
+                    data = line.split(',')
+                    process_id = data[0]
+                    process_name = data[1]
+                    status = data[2]
+                    start_time = data[3]
+
+                    # Check if the process name is in the list of malicious processes
+                    if [process_name] in malicious_processes:
+                        print("Found malicious process:", process_name)
+                        writer.writerow({'PROCESSID': process_id, 'PROCESSNAME': process_name, 'STATUS': status, 'STARTTIME': start_time})
+                    else:
+                        print("Process not found in malicious_processes.csv:", process_name)
+
+# Call the function
+check_malicious_processes()
